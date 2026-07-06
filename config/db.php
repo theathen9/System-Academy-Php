e74b7786b389948270b8db14a80bb781df2169c3
theathen9 <theathen36@gmail.com>
2026-07-06 23:38:04 +0700

    Refactor db.php for PostgreSQL connection using PDO
    Updated database connection details and switched to PDO.

--- config/db.php
+++ config/db.php
@@ -1,25 +1,48 @@
 <?php
 // ./config/db.php
-
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+// Database credentials (replace with your actual values)
+// $servername = "localhost";   // Hosting mySQL
+// $username   = "root";        // Your DB username
+// $password   = "";            // Your DB password            
+// $db         = "systemacademy";   // Your DB name
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $db);
 
-$host = "dpg-d953l9kvikkc73d87fo0-a";
-$port = "5432";
+// // Check connection
+// if ($conn->connect_error) {
+//     die("❌ Database connection failed: " . $conn->connect_error);
+// }
+
+// // Optional: Set charset to UTF-8 (recommended)
+// $conn->set_charset("utf8");
+
+
+// ./config/db.php
+
+$host = "dpg-d953l9kvikkc73d87fo0-a.oregon-postgres.render.com";
+$port = 5432;
 $dbname = "systemacademy";
-$user = "systemacademy_user";
+$username = "systemacademy_user";
 $password = "nvO3JsebMJWRG0U0rJ3MU3oGNsPsy35J";
+
 
 try {
     $conn = new PDO(
         "pgsql:host=$host;port=$port;dbname=$dbname",
-        $user,
+        $username,
         $password
     );
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // optional (safe)
+    $conn->exec("SET client_encoding TO 'UTF8'");
+
+    // echo "✅ Database connected";
+
 } catch (PDOException $e) {
-    die("❌ Database connection failed: " . $e->getMessage());
+    die("❌ Connection failed: " . $e->getMessage());
 }
