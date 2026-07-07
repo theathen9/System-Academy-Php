9cfa544634e89f7ddd043edf2b05ce895035039a
theathen9 <theathen36@gmail.com>
2026-07-07 11:17:39 +0700

    Change BASE_URL to new path

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 // ./config/app
-define('BASE_URL', '/system-academy-php.onrender.com');
+define('BASE_URL', '/html');
 define('APP_SECRET', 'SECRET_KEY');
 define('BASE_INSTITUTE', '/system-management/admin/institute');
 define('BASE_SIS', '/system-management/admin/sis');
